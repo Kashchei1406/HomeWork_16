476c070efd1c0caa935655ddd2329b5d14e9571c
Kashchei <375296218881g@gmail.com>
2020-09-10 19:39:33 +0300

    Home Work 16

---
+++ src/main/java/HomeWork16/Calculator.java
@@ -0,0 +1,29 @@
+package HomeWork16;
+
+public class Calculator {
+
+    public int integerPars(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public int addition(Integer a, Integer b) {
+
+        return a + b;
+    }
+
+    public int multiplication(Integer a, Integer b) {
+
+        return a * b;
+    }
+
+    public double division(Integer a, Integer b) {
+
+        return a / b;
+    }
+
+    public int subtraction(Integer a, Integer b) {
+        return a - b;
+    }
+
+
+}
---
+++ src/test/java/HomeWork16/CalculatorTest.java
@@ -0,0 +1,108 @@
+package HomeWork16;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    public Calculator calculator = new Calculator();
+    private final int a = 3;
+    private final int b = 5;
+
+    @Test
+    public void testAddition() {
+
+        // GIVEN
+        int intBefore = 0;
+
+        // WHEN
+        long intAfter = calculator.addition(a, b);
+        long intNotNull = calculator.addition(intBefore, b);
+
+        //THEN
+        assertEquals(a + b, intAfter);
+        assertNotSame(intBefore, intNotNull);
+
+    }
+
+    @Test
+    public void testMultiplication() {
+
+        //GIVEN
+        int intBefore = 10;
+
+        //WHEN
+        int result = calculator.multiplication(a, b);
+        int intAfter = calculator.multiplication(intBefore, a);
+
+        //THEN
+        assertEquals(15, result);
+        assertTrue(intBefore < intAfter);
+
+    }
+
+    @Test
+    public void testDivision() {
+
+        //GIVEN
+        int intBefore = 5;
+
+        //WHEN
+        double result = calculator.division(6, 3);
+        double intAfter = calculator.division(intBefore, a);
+
+        //THEN
+        assertEquals(2, result, 0.1);
+        assertTrue(intBefore > intAfter);
+
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+
+        //WHEN
+        calculator.division(a, 0);
+
+    }
+
+    @Test
+    public void testSubtraction() {
+
+        //GIVEN
+        int intBefore = 2;
+
+        //WHEN
+        int result = calculator.subtraction(b, a);
+        int intAfter = calculator.subtraction(intBefore, a);
+
+        //THEN
+        assertEquals(2, result);
+        assertTrue(intAfter < intBefore);
+    }
+
+    @Test
+    public void testIntegerPars() {
+        //GIVEN
+        String str = "1";
+
+        //WHEN
+        int resultIntegerPars = calculator.integerPars(str);
+
+        //THEN
+        assertEquals(1, resultIntegerPars);
+        
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testIntegerPars_negativeFlow() {
+        //GIVEN
+        String str = "1a";
+
+        //WHEN
+        int resultIntegerPars = calculator.integerPars(str);
+
+        //THEN
+        assertEquals(1, resultIntegerPars);
+    }
+}
